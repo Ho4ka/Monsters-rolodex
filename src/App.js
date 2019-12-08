e7820cc606b7c4bbe5ba0ddd33e6e589569d45fa
Ho4ka <m.domaretsky@gmail.com>
2019-12-08 14:43:09 +0200

    Added card-list component and fetch monsters

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import { CardList } from "./components/card-list/card-list.component";
+
 class App extends Component {
 
     constructor() {
         super();
 
         this.state = {
-            monsters: [
-                {
-                    name: 'Frankenstein',
-                    id: 'm1'
-                },
-                {
-                    name: 'Dracula',
-                    id: 'm2'
-                },
-                {
-                    name: 'Zombie',
-                    id: 'm3'
-                }
-            ]
+            monsters: []
         }
+    }
+
+    componentDidMount() {
+        fetch('http://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ monsters: users }));
     }
 
     render() {
         return (
             <div className="App">
+                <CardList>
                 {
                     this.state.monsters.map(monster =>{
-                        return <p key={monster.id}>{ monster.name }</p>
+                        return <h1 key={ monster.id }>{ monster.name }</h1>
                     })
                 }
+                </CardList>
 
             </div>
         );
